6ae0288011e60cb0a845e6e538a6c08f0c6f6c12
shawshank718 <singarisathwik007@gmail.com>
2016-04-02 21:49:19 +1100

    Added 404
    Added 404. Reduced some duplication in html.

--- main.py
+++ main.py
@@ -106,5 +106,10 @@ def dashboard():
     return render_template('dashboard.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
 if __name__ == '__main__':
     app.run()
