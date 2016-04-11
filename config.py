26ef17fd87c4bd862bdc6b04dd88c776ecc68dac
shawshank718 <singarisathwik007@gmail.com>
2016-04-11 20:56:54 +1000

    Added sqlite db

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = '_@awb+dp6x96^m+rcsn7^qh9xl1b)=tm76=jgjue_io2$ycu1m'
-    SQLALCHEMY_DATABASE_URI = 'mysql://bluegarden:XN4rcpxxwXdcDm2E@127.0.0.1:3306/bluegarden'
+    # SQLALCHEMY_DATABASE_URI = 'mysql://bluegarden:XN4rcpxxwXdcDm2E@127.0.0.1:3306/bluegarden'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///bluegarden.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
