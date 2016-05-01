f8b0e1a50d4e55ff052291f46bbb2bf74ca5904b
jloc8749 <jloc8749@uni.syd.edu.au>
2016-05-01 15:29:25 +1000

    added BDD tests for add farms user stories

--- features/addFarm.feature
+++ features/addFarm.feature
@@ -3,25 +3,17 @@ Feature: Farmer user can add a farm which they work on
     I want to register farms which I work on
     so that I can track the crops and produce related to that farm
     
-    Scenario Outline: Add farm to "My Farms" list
+    Scenario: Add farm to "My Farms" list
     Given at the sell page
-    When a farmer submits valid farm <name> and <address>
-    Then the the new farm name: <name> is displayed in the My Farms list
-    Examples:
-        |name|
-        |Marrickville Community Garden|
+    When a farmer submits valid farm name and address
+    Then the new farm name is displayed in the My Farms list
 
-    Scenario Outline: Farmer adds existing farm -->Fail
-    Given at the sell page
-    When a farmer submits an invalid (already existing) farm <name> and <address>
+    Scenario: Farmer adds existing farm -->Fail
+    Given logged in at the sell page
+    When a farmer submits an invalid (already existing) farm name
     Then the the error: "Already Exists" is returned
-    Examples:
-        |name|
-        |High St Gardening society|
-
     
-    Scenario Outline: User is not a farmer (yet to add a farm they work on)
+    Scenario: User is not a farmer (yet to add a farm they work on)
     Given The user has not registered any existing farms
     When the user views the sell page
     Then the the error: "You dont have any farms yet." is returned
-
--- features/steps/addFarmsteps.py
+++ features/steps/addFarmsteps.py
@@ -1,28 +1,48 @@
-import behave
+from behave import *
 import re
 
 @given('at the sell page')
 def step_impl(context):
+    context.browser.get(context.address + "/login")
+    login(context, 'farmer_j01@gmail.com', 'louise1993')
     context.browser.get(context.address + "/sell")
-    MyFarms_found = re.search("My Farms", context.browser.page_source)
-    assert MyFarms_found
+    assert 'Add Farm' in context.browser.page_source
 
-
-@when('a farmer submits valid farm <name> and <address>')
+@when('a farmer submits valid farm name and address')
 def step_impl(context):
-    #first login?
-    name = "Marrickville Community Garden"
     add_farm(context, "Marrickville Community Garden", "1 Marry St", "", "Marickville", "NSW", "Australia", "2009")
-    rv = context.client.get('/sell')
-    assert b'Marrickville Community Garden' in rv.data
 
 
-@then('the the new farm name: <name> is displayed in the My Farms list')
+
+@then('the new farm name is displayed in the My Farms list')
+def step_impl(context):
+    assert 'Marrickville Community Garden' in context.browser.page_source
+
+@given('logged in at the sell page')
+def step_impl(context):
+    assert True
+
+@when('a farmer submits an invalid (already existing) farm name')
+def step_impl(context):
+    add_farm(context, "Marrickville Community Garden", "1 Marry St", "", "Marickville", "NSW", "Australia", "2009")
+
+@then('the the error: "Already Exists" is returned')
+def step_impl(context):
+    assert 'Already Exists' in context.browser.page_source
+    
+@given('The user has not registered any existing farms')
+def step_impl(context):
+    context.browser.get(context.address + "/register")
+    register(context, "IAMANEW", "USER", "joe@nofarms.yet", "pineapple")
+
+@when('the user views the sell page')
 def step_impl(context):
     context.browser.get(context.address + "/sell")
-    assert 'Marrickville Community Garden' in context.browser.page_source
-    
-    
+
+@then('the the error: "You dont have any farms yet." is returned')
+def step_impl(context):
+    assert 'You dont have any farms yet.' in context.browser.page_source
+
 def add_farm(context, name, address1, address2, city, state, country, postcode):
     
     context.browser.find_element_by_name('name').send_keys(name)
@@ -32,5 +52,22 @@ def add_farm(context, name, address1, address2, city, state, country, postcode):
     context.browser.find_element_by_name('state').send_keys(state)
     context.browser.find_element_by_name('country').send_keys(country)
     context.browser.find_element_by_name('postcode').send_keys(postcode)
-    context.browser.find_element_by_id("form").submit()
+    context.browser.find_element_by_id("form").submit()   
      
+def login(context, email, password):
+    email_field = context.browser.find_element_by_id("email")
+    password_field = context.browser.find_element_by_id("password")
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    email_field.submit()
+    
+def register(context, first_name, last_name, email, password):
+    firstname_field = context.browser.find_element_by_id("firstname")
+    lastname_field = context.browser.find_element_by_id("lastname")
+    email_field = context.browser.find_element_by_id("email")
+    password_field = context.browser.find_element_by_id("password")
+    firstname_field.send_keys(first_name)
+    lastname_field.send_keys(last_name)
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    email_field.submit()
