2f5f51dcd52e27171c98d7019add45d0b7729f62
cruxyoung <cruxyoung@hotmail.com>
2016-05-05 17:40:21 -0700

    fixed bdd

--- features/steps/addcrop.py
+++ features/steps/addcrop.py
@@ -6,32 +6,39 @@ import re
 @given('at the crop screen')  
 def step_impl(context):
     context.browser.get(context.address + "/login")
-    login(context, 'bbaggins@lotr.com', 'bilbobaggins')
+    login(context, 'singarisathwik007@gmail.com', 'dm08b048')
     context.browser.get(context.address + "/addcrop")
     
-    addcrop_found = re.search("add crop", context.browser.page_source)
-    assert addcrop_found
+    #addcrop_found = re.search("add crop", context.browser.page_source)
+    assert 'add crop' in context.browser.page_source
     
-@when('a farmer submit the <id> <cropname>, <growstate> and <farm_id>')
-def step_impl(context,id, cropname,growstate, farm_id):
-    
-    addcrop(context,"1",  "corn", "plant", "1")
-    
-@then('the system should return "{text}"')
+@when('a farmer submit the id cropname, growstate and farm_id')
 def step_impl(context):
-    assert 'You success added crop' in context.context.browser.page_source    
+    addcrop(context,"3",  "corn", "plant", "1")
+    assert context.browser.page_source
+    
+@then('the system should return "You success added crop"')
+def step_impl(context):
+    assert 'You success added crop' in context.browser.page_source    
 
 
 
-def addcrop(context, id, crop_name, grow_state, farm_id):
+def addcrop(context, id, cropname, growstate, farmid):
     id_field = context.browser.find_element_by_id("id")
-    cropname_field = context.browser.find_element_by_id("crop_name")
-    growstate_field = context.browser.find_element_by_id("grow_state")
-    farmid_field = context.browser.find_element_by_id("farm_id")
+    cropname_field = context.browser.find_element_by_id("cropname")
+    growstate_field = context.browser.find_element_by_id("growstate")
+    farmid_field = context.browser.find_element_by_id("farmid")
     id_field.send_keys(id)
-    cropname_field.send_keys(crop_name)
-    growstate_field.send_keys(grow_state)
-    farmid_field.send_keys(farm_id)
+    cropname_field.send_keys(cropname)
+    growstate_field.send_keys(growstate)
+    farmid_field.send_keys(farmid)
+    id_field.submit()
+    '''context.browser.find_element_by_name('id').send_keys(id)
+    context.browser.find_element_by_name('cropname').send_keys(cropname)
+    context.browser.find_element_by_name('growstate').send_keys(growstate)
+    context.browser.find_element_by_name('farmid').send_keys(farmid)
+    context.browser.find_element_by_name(id).submit()'''
+    #context.response = context.browser.page_source
     
     
     
