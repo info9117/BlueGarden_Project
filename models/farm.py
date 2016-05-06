9920c67c3230c184c21da13c32f140a8f525493c
cruxyoung <cruxyoung@hotmail.com>
2016-05-05 19:13:45 -0700

    change db

--- models/farm.py
+++ models/farm.py
@@ -9,20 +9,10 @@ class Farm(db.Model):
 
 
 
-   ''' def __init__(self, name, address_id):
-        self.name = name
-        self.address_id = address_id
-
-    #produce_id = db.Column('produce_id', db.Integer, db.ForeignKey('produces.id'))'''
-
+   
 
     def __init__(self, name, address_id):
         self.name = name
         self.address_id = address_id
 
-    produce_id = db.Column('produce_id', db.Integer, db.ForeignKey('produces.id'))
-
-    def __init__(self, name, address_id, produce_id):
-        self.name = name
-        self.address_id = address_id
-        self.produce_id = produce_id
+  
