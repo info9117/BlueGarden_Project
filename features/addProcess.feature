6d94207c0af2d9dbc54f48d9f2e4cdc0e43fd55a
Chris Jardine <cjar7870@uni.sydney.edu.au>
2016-05-28 13:42:06 +1000

    added null process tests

--- features/addProcess.feature
+++ features/addProcess.feature
@@ -8,12 +8,22 @@ Feature: Farmer can create a new process
     When a farmer creates a new process
     Then the new process is recorded
 
-    Scenario: Linke from process to activity
+    Scenario: Link from process to activity
     Given at the process page
     When I select an exting process
     Then I  will be directed to the Activity page and the process will be selected
 
     Scenario: Add a new activity to a process
-    Given at the activity page
+    Given at the activity page for sathwick user
     When I select an exting process and add an activity
     Then I will be shown the activity added to the process
+
+    Scenario: Cannot add an blank process
+    Given at the activity page for sathwick user
+    When a farmer creates a new process with no name
+    Then I will be shown error
+
+    Scenario: Cannot add an blank process description
+    Given at the activity page for sathwick user
+    When a farmer creates a new process with no description
+    Then I will be shown error
--- features/steps/addProcess.py
+++ features/steps/addProcess.py
@@ -8,7 +8,8 @@ def step_impl(context):
     context.browser.get(context.address + '/process')
     assert "Add a new Process" in context.browser.page_source
 
-@given('at the activity page')
+
+@given('at the activity page for sathwick user')
 def step_impl(context):
     context.browser.get(context.address + "/login")
     login(context, 'singarisathwik007@gmail.com', 'dm08b048')
@@ -17,7 +18,7 @@ def step_impl(context):
 
 @when('I select an exting process and add an activity')
 def step_impl(context):
-    addActivity(context, 'Grow Spaghetti', 'Dig a hole')
+    addActivity(context, 'Dig a hole')
 
 @when('a farmer creates a new process')
 def step_impl(context):
@@ -27,14 +28,29 @@ def step_impl(context):
 def step_impl(context):
     context.browser.get(context.address + '/activity/1/add')
 
+@when('a farmer creates a new process with no name')
+def step_impl(context):
+    addProcess(context, "","The name of this process is classified")
+
+@when('a farmer creates a new process with no description')
+def step_impl(context):
+    addProcess(context, "Null process","")
+
+
 @then('I  will be directed to the Activity page and the process will be selected')
 def step_impl(context):
     assert "Grow Spaghetti" in context.browser.page_source
 
+@then('I will be shown the activity added to the process')
+def step_impl(context):
+    assert "Dig a hole" in context.browser.page_source
 
 @then('the new process is recorded')
 def step_impl(context):
     assert 'Grow Beef'in context.browser.page_source
+
+
+
 
 def login(context, email, password):
     email_field = context.browser.find_element_by_id("email")
@@ -48,7 +64,6 @@ def addProcess(context, process_name, process_description):
     context.browser.find_element_by_name('process_description').send_keys(process_description)
     context.browser.find_element_by_id("form").submit()
 
-def addActivity(context, process_name, activity_description):
-    context.browser.find_element_by_name('process').send_keys(process_name)
+def addActivity(context, activity_description):
     context.browser.find_element_by_name('description').send_keys(activity_description)
     context.browser.find_element_by_id("form").submit()
