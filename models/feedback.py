1592581de7575ed827012420c4067e6d7fda4341
hmktzt <hmktzt@me.com>
2016-05-30 14:40:36 +1000

    feedback form

---
+++ models/feedback.py
@@ -0,0 +1,18 @@
+from shared import db
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column('id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(255), nullable=False)
+    email = db.Column('email', db.String(255), nullable=False)
+    subject = db.Column('subject', db.String(255))
+    message = db.Column('message', db.String(255))
+    
+
+    def __init__(self, username, email, subject, message):
+        self.username = username
+        self.email = email
+        self.subject = subject
+        self.message = message
+
